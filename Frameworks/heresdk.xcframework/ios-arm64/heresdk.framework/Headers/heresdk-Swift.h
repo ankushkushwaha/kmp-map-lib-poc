d7d2a99e7f5da6e1e362bc3ac2919a37453b1bc2
ankushkushwaha <ankush7377@gmail.com>
2024-12-20 23:51:01 +0530

    Add here sdk

---
+++ Frameworks/heresdk.xcframework/ios-arm64/heresdk.framework/Headers/heresdk-Swift.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e234b7b5186dc016ed38e1aa3f3be7b7bfc193c80e17466604620fc21fa8616
+size 10897
---
+++ Frameworks/heresdk.xcframework/ios-arm64_x86_64-simulator/heresdk.framework/Headers/heresdk-Swift.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42d17c4fddf51008f58418f512f27b2bab8371a126dde2093f64b76fa1a2e11e
+size 21739
