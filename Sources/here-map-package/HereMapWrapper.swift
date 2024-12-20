6ac19a03ba103dffc099f4c95d091cb548cc0fb5
ankushkushwaha <ankush7377@gmail.com>
2024-12-20 18:32:18 +0530

    Initial commit

---
+++ Sources/here-map-package/HereMapWrapper.swift
@@ -0,0 +1,25 @@
+//
+//  HereMapWrapper.swift
+//  MyPackage
+//
+//  Created by Ankush Kushwaha on 20/12/24.
+//
+
+import Foundation
+
+public struct HereMapWrapper: MapController {
+    public init() {}
+
+    func darwRoutes(points: [Int]) {
+        print("darwRoutes for points \(points)")
+    }
+    
+    func addPin(at point: Int) {
+        print("addPin for point \(point)")
+    }
+}
+
+protocol MapController {
+    func darwRoutes(points: [Int])
+    func addPin(at point: Int)
+}
