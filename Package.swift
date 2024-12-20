3c3a2c873b8eaa73da034b9ab488b9bbad0965d8
ankushkushwaha <ankush7377@gmail.com>
2024-12-21 01:13:18 +0530

    Support ios 15 and above

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "here-map-package",
+    platforms: [
+            .iOS(.v15) // Specify the minimum supported iOS version here
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
