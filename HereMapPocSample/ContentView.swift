917dc71628a9c2bb465bac8e3771128378abd974
ankushkushwaha <ankush7377@gmail.com>
2024-12-21 00:27:52 +0530

    Add here package in sample project

--- HereMapPocSample/ContentView.swift
+++ HereMapPocSample/ContentView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+import here_map_package
 
 struct ContentView: View {
+    
+    let hereMap = HereMapWrapper(accessKeyID: ACCESS_KEY_ID, accessKeySecret: ACCESS_KEY_SECRET)
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Show map") {
+                hereMap.addPin(at: 2)
+            }
         }
         .padding()
     }
@@ -22,3 +28,7 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+ let ACCESS_KEY_ID = "nzK5NTVRrBB7zoixWk5puw"
+ let ACCESS_KEY_SECRET = "VZ5aGmGSZ9zFQR87D8eH6tEFWv2BhoKp4D-YX0C4kEEykIEUQvh_GnrHoW5X_GWzl3cN118kI_3YX0Dlk_4TVQ"
