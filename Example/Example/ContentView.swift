e17b792b36355c1327a19239fe365115e6957b75
ankushkushwaha <ankush7377@gmail.com>
2025-01-24 15:00:00 +0530

    Shift constants to one place

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -130,7 +130,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-
- let ACCESS_KEY_ID = "nzK5NTVRrBB7zoixWk5puw"
- let ACCESS_KEY_SECRET = "VZ5aGmGSZ9zFQR87D8eH6tEFWv2BhoKp4D-YX0C4kEEykIEUQvh_GnrHoW5X_GWzl3cN118kI_3YX0Dlk_4TVQ"
