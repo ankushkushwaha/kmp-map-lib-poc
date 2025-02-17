612a1c4dfa94283e93576e5a2ef44f13b0079197
ankushkushwaha <ankush7377@gmail.com>
2025-02-17 18:40:38 +0530

    Add example project

---
+++ ExampleGoogleMap/ExampleGoogleMap/ExampleGoogleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleGoogleMapApp.swift
+//  ExampleGoogleMap
+//
+//  Created by Ankush Kushwaha on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleGoogleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
