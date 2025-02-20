6ff16f1ec9742d1a3bfc29926d3c3bcb82cece0a
ankushkushwaha <ankush7377@gmail.com>
2025-02-20 18:29:57 +0530

    Update cordinates

--- ExampleGoogleMap/ExampleGoogleMap/ContentView.swift
+++ ExampleGoogleMap/ExampleGoogleMap/ContentView.swift
@@ -74,21 +74,21 @@ struct ContentView: View {
             Button("Add Route") {
                 
                 let points: [CLLocationCoordinate2D] = [
-                    CLLocationCoordinate2D(latitude: 52.5505, longitude: 13.3704), // Gesundbrunnen
+                    CLLocationCoordinate2D(latitude: 52.5505, longitude: 13.3704), // Gesundbrunnen (North)
                     CLLocationCoordinate2D(latitude: 52.5426, longitude: 13.3499), // Mauerpark
                     CLLocationCoordinate2D(latitude: 52.5294, longitude: 13.4134), // Hackescher Markt
                     CLLocationCoordinate2D(latitude: 52.5208, longitude: 13.4094), // Alexanderplatz
                     CLLocationCoordinate2D(latitude: 52.5186, longitude: 13.3762), // Reichstag Building
                     CLLocationCoordinate2D(latitude: 52.5163, longitude: 13.3777), // Brandenburg Gate
-                    CLLocationCoordinate2D(latitude: 52.5097, longitude: 13.3758), // Tiergarten
-                    CLLocationCoordinate2D(latitude: 52.5037, longitude: 13.3769), // Potsdamer Platz
                     CLLocationCoordinate2D(latitude: 52.5076, longitude: 13.3904), // Checkpoint Charlie
-                    CLLocationCoordinate2D(latitude: 52.4958, longitude: 13.3051), // Charlottenburg Palace
-                    CLLocationCoordinate2D(latitude: 52.4854, longitude: 13.4443), // Treptower Park
-                    CLLocationCoordinate2D(latitude: 52.4731, longitude: 13.4220), // Tempelhofer Feld
+                    CLLocationCoordinate2D(latitude: 52.5037, longitude: 13.3769), // Potsdamer Platz
+                    CLLocationCoordinate2D(latitude: 52.5097, longitude: 13.3758), // Tiergarten (Moved closer)
+                    CLLocationCoordinate2D(latitude: 52.4958, longitude: 13.3051), // Charlottenburg Palace (West)
                     CLLocationCoordinate2D(latitude: 52.4701, longitude: 13.3872), // Schöneberg
+                    CLLocationCoordinate2D(latitude: 52.4731, longitude: 13.4220), // Tempelhofer Feld
+                    CLLocationCoordinate2D(latitude: 52.4854, longitude: 13.4443), // Treptower Park (Moved to end)
                     CLLocationCoordinate2D(latitude: 52.4617, longitude: 13.3722), // Rathaus Steglitz
-                    CLLocationCoordinate2D(latitude: 52.4550, longitude: 13.2901)  // Wannsee
+                    CLLocationCoordinate2D(latitude: 52.4550, longitude: 13.2901)  // Wannsee (Furthest South-West)
                 ]
 
                 GoogleMapWrapper.shared?.drawRoute(points)
