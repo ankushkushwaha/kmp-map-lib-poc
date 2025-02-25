270393903d6df846f746a1228898c58778a91553
ankushkushwaha <ankush7377@gmail.com>
2025-02-25 14:17:00 +0530

    Add route and clear map button

--- HereMapPocSample/ContentView.swift
+++ HereMapPocSample/ContentView.swift
@@ -12,10 +12,6 @@ import heresdk
 struct ContentView: View {
     @State private var mapView = HereMapWrapper.shared!.mapView
     
-    init() {
-//        self.mapView = HereMapWrapper.shared?.mapView ?? MapView()
-    }
-    
     var body: some View {
         VStack {
             
@@ -27,14 +23,22 @@ struct ContentView: View {
                     ),
                     image: UIImage(systemName: "car.fill")!
                 )
-                
-                HereMapWrapper.shared?.addMarker(
-                    GeoCoordinates(
-                        latitude: 52.520798,
-                        longitude: 13.409408
-                    ),
-                    image: UIImage(systemName: "car.fill")!
-                )
+                HereMapWrapper.shared?.moveCamera(GeoCoordinates(
+                    latitude: 52.520798,
+                    longitude: 13.409408
+                ))
+            }
+            
+            Button("Add Route") {
+                let startGeoCoordinates = GeoCoordinates(latitude: 52.51087113766646, longitude: 13.396939881800781)
+                let destinationGeoCoordinates = GeoCoordinates(latitude: 52.53595152570505, longitude: 13.425996387349484)
+                HereMapWrapper.shared?.darwRoute(start: startGeoCoordinates, end: destinationGeoCoordinates)
+                    
+                HereMapWrapper.shared?.moveCamera(startGeoCoordinates)
+            }
+            
+            Button("Clear Map") {
+                HereMapWrapper.shared?.clearMap()
             }
             
             MapViewUIRepresentable(mapView: $mapView)
